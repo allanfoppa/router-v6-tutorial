dab50dd6675e456aec16b1b646ac94396e048f36
Allan Foppa <allanfoppa@hotmail.com>
2022-01-11 15:05:50 -0300

    Tutorial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tutorial React Router v6 in
+
+https://reactrouter.com/docs/en/v6/getting-started/tutorial
---
+++ src/index.js
@@ -0,0 +1,42 @@
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import App from "./App";
+
+import Expenses from "./view/Expenses.view";
+import Invoices from "./view/Invoices.view";
+import Invoice from "./view/Invoice.view";
+
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />}>
+          <Route
+            index
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>Select an invoice</p>
+              </main>
+            }
+          />
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
+      </Route>
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>There's nothing here!</p>
+          </main>
+        }
+      />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+);
